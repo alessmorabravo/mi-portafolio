6b6e1adb5ba899575f43ed853f8c80289d390e82
Alessandro Mora <morabravoalessandro@gmail.com>
2025-02-27 11:18:56 -0500

    Agrego portafolio de páginas web
    Se agregan archivos del portafolio de páginas web desarrolladas con HTML, CSS y JavaScript. Incluye diseños responsivos con Bootstrap.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const app = document.getElementById('typewriter');
+
+const typewriter = new Typewriter(app, {
+    loop: true,
+    delay: 100
+});
+
+typewriter
+.typeString('- Well + Good')
+.pauseFor(900)
+.start();
